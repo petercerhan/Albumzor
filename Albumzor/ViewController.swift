d5f6d13aad60de3afd082cbbc8b6184195c32300
petercerhan <petercerhan@gmail.com>
2017-03-12 14:42:41 -0500

    Initial Commit

---
+++ Albumzor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Albumzor
+//
+//  Created by Peter Cerhan on 3/12/17.
+//  Copyright © 2017 Peter Cerhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
