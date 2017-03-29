54d1fd3a2be3bbfc698f36c73ce4e8fe02270faf
petercerhan <petercerhan@gmail.com>
2017-03-28 19:56:46 -0500

    Show tracks on album details view

--- Albumzor/AlbumDetailsViewController.swift
+++ Albumzor/AlbumDetailsViewController.swift
@@ -11,24 +11,27 @@ import UIKit
 class AlbumDetailsViewController: UIViewController {
     
     @IBOutlet var imageView: UIImageView!
+    @IBOutlet var tableView: UITableView!
     
     var tracks: [Track]?
     
     var albumImage: UIImage!
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Default")
         
-        imageView.image = albumImage
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+        super.viewDidLoad()
         if let tracks = tracks {
             for track in tracks {
                 print("track \(track.track): \(track.name)")
             }
         }
+        imageView.image = albumImage
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
     }
     
     @IBAction func back() {
@@ -36,3 +39,30 @@ class AlbumDetailsViewController: UIViewController {
     }
 
 }
+
+extension AlbumDetailsViewController: UITableViewDelegate {
+    
+    
+    
+}
+
+extension AlbumDetailsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let tracks = tracks {
+            return tracks.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Default")!
+        
+        cell.textLabel!.text = tracks![indexPath.item].name!
+
+        return cell
+    }
+    
+}
+
