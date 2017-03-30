7ad368864b8f67282758d0a2f534822b57bd16a7
petercerhan <petercerhan@gmail.com>
2017-03-29 20:55:15 -0500

    Custom layout code for artist collection view

--- Albumzor/ChooseArtistViewController.swift
+++ Albumzor/ChooseArtistViewController.swift
@@ -16,6 +16,9 @@ class ChooseArtistViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let layout = collectionView.collectionViewLayout as? ArtistCollectionViewLayout {
+            layout.delegate = self
+        }
     }
     
     @IBAction func done() {
@@ -49,6 +52,17 @@ extension ChooseArtistViewController: UICollectionViewDataSource {
 
 extension ChooseArtistViewController: UICollectionViewDelegateFlowLayout {
     
+}
+
+extension ChooseArtistViewController: ArtistCollectionViewLayoutDelegate {
+    func collectionView(_ collectionView: UICollectionView, sizeForLabelAtIndexPath path: IndexPath) -> CGSize {
+        let label = UILabel()
+        label.text = ChooseArtistViewController.suggestedArtists[path.item]
+        label.font = UIFont.systemFont(ofSize: 18.0)
+        label.frame = CGRect(x: 0, y: 0, width: 1, height: 1)
+        label.sizeToFit()
+        return label.frame.size
+    }
 }
 
 //MARK:- Artist Data
