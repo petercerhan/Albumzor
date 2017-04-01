d8a1d9b8f978715942b1ea4abe02ce7fae857738
petercerhan <petercerhan@gmail.com>
2017-04-01 14:30:06 -0500

    Add CGDraggableView and CGOverlayView to project

---
+++ Albumzor/CGDraggableView.swift
@@ -0,0 +1,141 @@
+//
+//  CGDraggableView.swift
+//  Albumzor
+//
+//  Created by Peter Cerhan on 4/1/17.
+//  Copyright © 2017 Peter Cerhan. All rights reserved.
+//
+
+import UIKit
+
+protocol CGDraggableViewDelegate {
+    func swipeComplete(direction: SwipeDirection)
+}
+
+class CGDraggableView: UIView {
+    
+    var panGestureRecognizer: UIPanGestureRecognizer!
+    var tapGestureRecognizer: UITapGestureRecognizer!
+    var originalPoint = CGPoint(x: 0, y: 0)
+    var overlayView: CGOverlayView!
+    var imageView: UIImageView!
+    var direction = SwipeDirection.none
+    
+    var delegate: CGDraggableViewDelegate?
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(dragged(gestureRecognizer:)))
+        self.addGestureRecognizer(panGestureRecognizer)
+        
+        tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped(gestureRecognizer:)))
+        self.addGestureRecognizer(tapGestureRecognizer)
+        
+        imageView = UIImageView(frame: bounds)
+        imageView.image = UIImage(named: "TestImage")
+        addSubview(imageView)
+        
+        overlayView = CGOverlayView(frame: bounds)
+        overlayView.alpha = 0
+        addSubview(overlayView)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(dragged(gestureRecognizer:)))
+        self.addGestureRecognizer(panGestureRecognizer)
+        
+        tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped(gestureRecognizer:)))
+        self.addGestureRecognizer(tapGestureRecognizer)
+        
+        imageView = UIImageView(frame: bounds)
+        imageView.image = UIImage(named: "TestImage")
+        addSubview(imageView)
+        
+        overlayView = CGOverlayView(frame: bounds)
+        overlayView.alpha = 0
+        addSubview(overlayView)
+    }
+    
+    func tapped(gestureRecognizer: UITapGestureRecognizer) {
+        print("tap detected")
+    }
+    
+    func dragged(gestureRecognizer: UIPanGestureRecognizer) {
+        let xDistance = gestureRecognizer.translation(in: self).x
+        let yDistance = gestureRecognizer.translation(in: self).y
+        
+        switch gestureRecognizer.state {
+        case .began:
+            originalPoint = center
+        case .changed:
+            let rotationStrength = min(xDistance / 375, 1.0)
+            let rotationAngle = 2 * CGFloat(M_PI) * rotationStrength / 16.0
+            let scaleStrength = CGFloat(1 - fabsf(Float(rotationStrength)) / 4.0)
+            let scale = max(scaleStrength, 0.93)
+            center = CGPoint(x: originalPoint.x + xDistance, y: originalPoint.y + yDistance)
+            let transform = CGAffineTransform(rotationAngle: rotationAngle)
+            let scaleTransform = transform.scaledBy(x: scale, y: scale)
+            self.transform = scaleTransform
+            
+            updateDirection(distance: xDistance)
+            
+        case .ended:
+            if fabsf(Float(xDistance)) > 100.0 {
+                completeSwipe()
+            } else {
+                resetViewPositionAndTransformations()
+            }
+        default:
+            break
+        }
+        
+    }
+    
+    func updateDirection(distance: CGFloat) {
+        
+        if distance > 0 {
+            direction = .right
+            overlayView.mode = .right
+        } else if distance <= 0 {
+            direction = .left
+            self.overlayView.mode = .left
+        }
+        
+        let overlayStrength = CGFloat(min(fabsf(Float(distance)) / 175, 0.4))
+        overlayView.alpha = overlayStrength
+    }
+    
+    func completeSwipe() {
+        
+        UIView.animate(withDuration: 0.1,
+                       animations: {
+                        
+                        if self.overlayView.mode == .right {
+                            self.center = CGPoint(x: self.center.x + 100.0, y: self.center.y + 0.0)
+                        } else {
+                            self.center = CGPoint(x: self.center.x - 100.0, y: self.center.y + 0.0)
+                        }
+                        self.alpha = 0
+        },
+                       completion: { _ in self.removeFromSuperview()
+                        if let delegate = self.delegate {
+                            delegate.swipeComplete(direction: self.direction)
+                        }
+        })
+    }
+    
+    func resetViewPositionAndTransformations() {
+        
+        UIView.animate(withDuration: 0.2, animations: {
+            self.center = self.originalPoint
+            self.transform = CGAffineTransform(rotationAngle: 0)
+            self.overlayView.alpha = 0
+        },
+                       completion: { _ in self.direction = .none})
+        
+    }
+    
+}
---
+++ Albumzor/CGOverlayView.swift
@@ -0,0 +1,52 @@
+//
+//  CGOverlayView.swift
+//  Albumzor
+//
+//  Created by Peter Cerhan on 4/1/17.
+//  Copyright © 2017 Peter Cerhan. All rights reserved.
+//
+
+import UIKit
+
+enum SwipeDirection: Int {
+    case none
+    case left
+    case right
+}
+
+class CGOverlayView: UIView {
+    
+    var mode: SwipeDirection = .none {
+        didSet {
+            if mode != oldValue {
+                if mode == .left {
+                    label.text = "left"
+                } else {
+                    label.text = "right"
+                }
+            }
+        }
+    }
+    
+    var label: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.white
+        label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 22.0)
+        label.text = "Long Sentence"
+        label.frame = CGRect(x: 0, y: 0, width: 1, height: 1)
+        label.sizeToFit()
+        addSubview(label)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    
+    
+}
