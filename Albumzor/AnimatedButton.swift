2a4ac032bf2fa81fd09faac3f0183ad00a37f065
petercerhan <petercerhan@gmail.com>
2017-04-19 18:04:24 -0500

    Reorganize home page and add animated button class

---
+++ Albumzor/AnimatedButton.swift
@@ -0,0 +1,34 @@
+//
+//  AnimatedButton.swift
+//  Albumzor
+//
+//  Created by Peter Cerhan on 4/18/17.
+//  Copyright © 2017 Peter Cerhan. All rights reserved.
+//
+
+import UIKit
+
+class AnimatedButton: UIButton {
+
+    var baseColor = UIColor.blue
+    var highlightedColor = UIColor.red
+    
+    var titleOffset = UIEdgeInsetsMake(CGFloat(3.0), CGFloat(3.0), CGFloat(0.0), CGFloat(0.0))
+    var zeroOffset = UIEdgeInsetsMake(CGFloat(0.0), CGFloat(0.0), CGFloat(0.0), CGFloat(0.0))
+    
+    override func beginTracking(_ touch: UITouch, with withEvent: UIEvent?) -> Bool {
+        backgroundColor = highlightedColor
+        titleEdgeInsets = titleOffset
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        backgroundColor = baseColor
+        titleEdgeInsets = zeroOffset
+    }
+    
+    override func cancelTracking(with event: UIEvent?) {
+        backgroundColor = baseColor
+        titleEdgeInsets = zeroOffset
+    }
+}
