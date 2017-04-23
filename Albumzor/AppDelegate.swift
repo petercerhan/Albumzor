72fc898fabc494ddd1cf54123a8fe4e6a1fa0928
petercerhan <petercerhan@gmail.com>
2017-04-23 18:24:40 -0500

    Set initial view controller programmatically

--- Albumzor/AppDelegate.swift
+++ Albumzor/AppDelegate.swift
@@ -21,6 +21,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = mainStoryboard.instantiateViewController(withIdentifier: "OpenSceneViewController")
+        
+        self.window?.rootViewController = vc
+        
+        self.window?.makeKeyAndVisible()
+        
         dataManager = DataManager()
         
 //        do {
