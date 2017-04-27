bfde07290871e41f6a9b6bea87476d9abac8e4a5
petercerhan <petercerhan@gmail.com>
2017-04-27 18:54:49 -0500

    Include UIViewController alert extension

---
+++ Albumzor/UIViewControllerAlerts.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewControllerAlerts.swift
+//  Albumzor
+//
+//  Created by Peter Cerhan on 4/27/17.
+//  Copyright © 2017 Peter Cerhan. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func alert(title: String?, message: String, buttonTitle: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: buttonTitle, style: .default, handler: nil)
+        alert.addAction(dismissAction)
+        present(alert, animated: true, completion: nil)
+    }
+}
