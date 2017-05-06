e1736166310cd25b635c1d93104ef04653ce092f
petercerhan <petercerhan@gmail.com>
2017-05-05 19:08:01 -0500

    Refactor app delegate

--- Albumzor/AppDelegate.swift
+++ Albumzor/AppDelegate.swift
@@ -23,12 +23,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        self.window = UIWindow(frame: UIScreen.main.bounds)
+        window = UIWindow(frame: UIScreen.main.bounds)
         let vc = MainContainerViewController()
         mainContainerViewController = vc
         
-        self.window?.rootViewController = vc
-        self.window?.makeKeyAndVisible()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
         
         dataManager = DataManager()
         
