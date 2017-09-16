074c21b08a4ed1c6af0996a30ec8946088fe362e
petercerhan <petercerhan@gmail.com>
2017-09-16 14:47:37 -0500

    Add RxSwift, RxCocoa, CocoaPods

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '9.0'
+
+target 'Albumzor' do
+  use_frameworks!
+
+pod 'RxSwift', '~> 3.4'
+pod 'RxCocoa', '~> 3.4'
+
+
+end
---
+++ Pods/Target Support Files/Pods-Albumzor/Pods-Albumzor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Albumzor : NSObject
+@end
+@implementation PodsDummy_Pods_Albumzor
+@end
