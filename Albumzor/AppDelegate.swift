bb3068e4b100e2fccbcea9c0dbede380a39b4b15
Peter Cerhan <petercerhan@Peters-MacBook-Pro.local>
2017-11-10 22:40:53 -0600

    remove unused code

--- Albumzor/AppDelegate.swift
+++ Albumzor/AppDelegate.swift
@@ -12,44 +12,20 @@ import RxSwift
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    let coreDataStack = CoreDataStack(modelName: "Model")!
-    var audioPlayer = AudioPlayer()
-    //initial/default settings
-    var userSettings = UserSettings(instructionsSeen: false, isSeeded: false, autoplay: true, albumSortType: 0)
-    var userProfile = UserProfile(userMarket: "None", spotifyConnected: false)
-    
-//    var mainContainerViewController: MainContainerViewController?
+//    let coreDataStack = CoreDataStack(modelName: "Model")!
+//    var audioPlayer = AudioPlayer()
     
     var window: UIWindow?
 
     var compositionRoot: CompositionRootProtocol!
     var rootCoordinator: RootCoordinator!
     
-    //Dev
-//    var authStateController: AuthStateController!
-    
-    
     //Mark: - Rx
-    //For dev purposes
+    //Dev
     let disposeBag = DisposeBag()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
-        //Reset "User settings"
-//        resetUserSettings()
-        
-        //Reset Database
-//        let coreDataUtilityService = CoreDataService(coreDataStack: CoreDataStack(modelName: "Model")!)
-//        coreDataUtilityService.resetDatabase()
-        
         compositionRoot = CompositionRoot()
-        
-        //Dev
-//        authStateController = compositionRoot.composeAuthStateController()
-        //
-//        authStateController.deleteSession()
-        //
-        
         rootCoordinator = compositionRoot.composeRootCoordinator()
         rootCoordinator.start()
         
@@ -71,71 +47,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return SpotifyAuthManager().open(url)
     }
     
-    //MARK: - Dev Utilities
-    func resetUserSettings() {
-        let userSettingsController = UserSettingsStateController(archiveService: UserDefaultsArchivingService())
-        userSettingsController.reset()
-    }
-
 }
-
-//MARK: - Manage user settings
-
-extension AppDelegate {
-    
-    func disconnectSpotify() {
-        SpotifyAuthManager().deleteSession();
-        UserDefaults.standard.removeObject(forKey: "SpotifySession")
-    }
-    
-    func deleteUserProfile() {
-        UserDefaults.standard.removeObject(forKey: "userProfile")
-    }
-    
-    func testForObject() {
-        if UserDefaults.standard.object(forKey: "SpotifySession") == nil {
-            print("did not find")
-        } else {
-            print("found")
-        }
-    }
-    
-    func loadUserSettings() {
-        if let data = UserDefaults.standard.object(forKey: "userSettings") as? Data,
-            let userSettings = NSKeyedUnarchiver.unarchiveObject(with: data) as? UserSettings {
-            
-            self.userSettings = userSettings
-        } else {
-            
-        }
-    }
-    
-    func saveUserSettings() {
-        let data = NSKeyedArchiver.archivedData(withRootObject: userSettings)
-        UserDefaults.standard.set(data, forKey: "userSettings")
-    }
-    
-    func loadUserProfile() {
-        if let data = UserDefaults.standard.object(forKey: "userProfile") as? Data,
-            let userProfile = NSKeyedUnarchiver.unarchiveObject(with: data) as? UserProfile {
-            
-            self.userProfile = userProfile
-        } else {
-            
-        }
-    }
-    
-    func saveUserProfile() {
-        let data = NSKeyedArchiver.archivedData(withRootObject: userProfile)
-        UserDefaults.standard.set(data, forKey: "userProfile")
-    }
-    
-    func resetUserProfile() {
-        userProfile = UserProfile(userMarket: "None", spotifyConnected: false)
-        saveUserProfile()
-    }
-}
-
-
-
-
